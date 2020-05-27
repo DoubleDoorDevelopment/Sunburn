cdd881206bcdb4acf920bfcc6f9aaa4d07cbd934
Claycorp <Claycorp@doubledoordev.net>
2020-05-27 16:46:43 -0500

    Apparently the 1.14 build never made it to Github, Oops.

---
+++ src/main/java/net/doubledoordev/sunburn/SunBurnConfig.java
@@ -0,0 +1,135 @@
+package net.doubledoordev.sunburn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.Pair;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
+import net.minecraftforge.common.ForgeConfigSpec.Builder;
+import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
+import net.minecraftforge.common.ForgeConfigSpec.IntValue;
+
+public class SunBurnConfig
+{
+
+    public static final SunBurnConfig.General GENERAL;
+    static final ForgeConfigSpec spec;
+
+    static
+    {
+        final Pair<General, ForgeConfigSpec> specPair = new Builder().configure(SunBurnConfig.General::new);
+        spec = specPair.getRight();
+        GENERAL = specPair.getLeft();
+    }
+
+    public static class General
+    {
+        public static List<? extends Integer> dimList()
+        {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(-1);
+
+            return list;
+        }
+
+        public IntValue burnTimeStart;
+        public IntValue burnTimeStop;
+        public IntValue lengthOfDay;
+        public IntValue lengthOfBurn;
+        public IntValue burnOverYLevel;
+        public IntValue waitToBurnTime;
+        public IntValue bypassDamge;
+
+        public BooleanValue playerMustSeeSky;
+        public BooleanValue wetStopsBurn;
+        public BooleanValue alwaysBurnOverYLevel;
+        public BooleanValue fullArmorBlocksBurn;
+        public BooleanValue hatsBlockBurn;
+        public BooleanValue bypassFireResist;
+        public BooleanValue whitelistOrBlacklist;
+
+        public ConfigValue<List<? extends Integer>> dimList;
+
+        General(Builder builder)
+        {
+            builder.comment("General configuration settings")
+                    .push("General");
+
+            dimList = builder
+                    .comment("Dimension list, whitelistOrBlacklist changes how this works!")
+                    .translation("sunburn.config.dimlist")
+                    .defineList("dimBlacklist", SunBurnConfig.General.dimList(), p -> p instanceof Integer);
+
+            burnTimeStart = builder
+                    .comment("Time of day burning starts in ticks. Normal day length is 0 to 23999.")
+                    .translation("sunburn.config.burntime.start")
+                    .defineInRange("burnTimeStart", 1000, 0, Integer.MAX_VALUE);
+
+            burnTimeStop = builder
+                    .comment("Time of day burning stops in ticks. Normal day length is 0 to 23999.")
+                    .translation("sunburn.config.burntime.stop")
+                    .defineInRange("burnTimeStop", 15000, 0, Integer.MAX_VALUE);
+
+            lengthOfDay = builder
+                    .comment("How long is one day. Normal day length 24000. DO NOT CHANGE UNLESS YOU CHANGE THE DAY LENGTH!")
+                    .translation("sunburn.config.daylength")
+                    .defineInRange("lengthOfDay", 24000, 0, Integer.MAX_VALUE);
+
+            lengthOfBurn = builder
+                    .comment("How long a player is set to burn for. This is how long the player will burn AFTER they are safe.")
+                    .translation("sunburn.config.burnlength")
+                    .defineInRange("lengthOfBurn", 1, 0, Integer.MAX_VALUE);
+
+            burnOverYLevel = builder
+                    .comment("If the player is below this Y level they will not burn, If they are above it they burn. This setting only works if playerMustSeeSky is false!")
+                    .translation("sunburn.config.burnylevel")
+                    .defineInRange("burnOverYLevel", 64, 0, Integer.MAX_VALUE);
+
+            waitToBurnTime = builder
+                    .comment("How long to wait in ticks before players can burn after spawning into the world, This includes logging in. 20 ticks = 1 second, 1200 ticks = 1 minute, 72000 ticks = 1 hour")
+                    .translation("sunburn.config.safetime")
+                    .defineInRange("waitToBurnTime", 600, 0, Integer.MAX_VALUE);
+
+            bypassDamge = builder
+                    .comment("How much damage the player takes from bypass damage.")
+                    .translation("sunburn.config.bypassdamage")
+                    .defineInRange("bypassDamge", 1, 0, Integer.MAX_VALUE);
+
+            playerMustSeeSky = builder
+                    .comment("The player only burns if they see the sky.")
+                    .translation("sunburn.config.playermustseesky")
+                    .define("playerMustSeeSky", true);
+
+            wetStopsBurn = builder
+                    .comment("The player being wet stops the burn. (Rain and water stops burning)")
+                    .translation("sunburn.config.wetStopsBurn")
+                    .define("wetStopsBurn", true);
+
+            alwaysBurnOverYLevel = builder
+                    .comment("Should players always burn over a certain Y level? This setting only works if playerMustSeeSky is false!")
+                    .translation("sunburn.config.burnovery")
+                    .define("alwaysBurnOverYLevel", true);
+
+            fullArmorBlocksBurn = builder
+                    .comment("Should players always burn over a certain Y level? This setting only works if playerMustSeeSky is false!")
+                    .translation("sunburn.config.armorblocksburn")
+                    .define("fullArmorBlocksBurn", false);
+
+            hatsBlockBurn = builder
+                    .comment("Should players always burn over a certain Y level? This setting only works if playerMustSeeSky is false!")
+                    .translation("sunburn.config.hatblocksburn")
+                    .define("hatsBlockBurn", true);
+
+            bypassFireResist = builder
+                    .comment("Changes the damage type to bypass fire resist potions/enchants.")
+                    .translation("sunburn.config.bypassfireresist")
+                    .define("bypassFireResist", false);
+
+            whitelistOrBlacklist = builder
+                    .comment("Changes how dimList is treated, True = Whitelist, False = Blacklist.")
+                    .translation("sunburn.config.whitelistorblacklist")
+                    .define("whitelistOrBlacklist", false);
+        }
+    }
+}
--- src/main/java/net/doubledoordev/sunburn/Sunburn.java
+++ src/main/java/net/doubledoordev/sunburn/Sunburn.java
@@ -1,34 +1,92 @@
 package net.doubledoordev.sunburn;
 
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.util.DamageSource;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.config.ModConfig;
 
-@Mod(
-        modid = Sunburn.MOD_ID,
-        name = Sunburn.MOD_NAME,
-        version = Sunburn.VERSION,
-        acceptableRemoteVersions = "*"
-)
+@Mod("sunburn")
 public class Sunburn
 {
-    public static final String MOD_ID = "sunburn";
-    public static final String MOD_NAME = "Sunburn";
-    public static final String VERSION = "2.0.0";
+    DamageSource damageSource = new DamageSource("sunburn").setDamageBypassesArmor().setDifficultyScaled();
 
-    /**
-     * This is the instance of your mod as created by Forge. It will never be null.
-     */
-    @Mod.Instance(MOD_ID)
-    public static Sunburn INSTANCE;
-
-    /**
-     * This is the first initialization event. Register tile entities here.
-     * The registry events below will have fired prior to entry to this method.
-     */
-    @Mod.EventHandler
-    public void preinit(FMLPreInitializationEvent event)
+    public Sunburn()
     {
-        MinecraftForge.EVENT_BUS.register(new EventHandlers());
+        MinecraftForge.EVENT_BUS.register(Sunburn.class);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, SunBurnConfig.spec);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onPlayerTickEvent(TickEvent.PlayerTickEvent event)
+    {
+        PlayerEntity player = event.player;
+        long time = player.getEntityWorld().getGameTime() % SunBurnConfig.GENERAL.lengthOfDay.get();
+
+        if (event.side.isClient() | event.phase == TickEvent.Phase.END) // Need to filter these out cause they cause issues.
+            return;
+
+        if (SunBurnConfig.GENERAL.wetStopsBurn.get() && player.isWet())
+            return;
+
+        if (player.isCreative() || player.isSpectator() || // If the player isn't in creative or spectator.
+                player.ticksExisted <= SunBurnConfig.GENERAL.waitToBurnTime.get() ||     // If the player entity is younger than the safe wait time.
+                time <= SunBurnConfig.GENERAL.burnTimeStart.get() || time >= SunBurnConfig.GENERAL.burnTimeStop.get()) // If the current time is below the burn start or the time is after the burn stop.
+            return;
+
+        // if hats block burn and the player is wearing one, ignore the burn.
+        if (SunBurnConfig.GENERAL.hatsBlockBurn.get() && player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem() instanceof ArmorItem)
+            return;
+
+        // if full armor blocks burn and the player is wearing something in each slot that is armor, ignore the burn.
+        if (SunBurnConfig.GENERAL.fullArmorBlocksBurn.get() && player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem() instanceof ArmorItem &&
+                player.getItemStackFromSlot(EquipmentSlotType.CHEST).getItem() instanceof ArmorItem &&
+                player.getItemStackFromSlot(EquipmentSlotType.LEGS).getItem() instanceof ArmorItem &&
+                player.getItemStackFromSlot(EquipmentSlotType.FEET).getItem() instanceof ArmorItem)
+            return;
+
+        for (int dim : SunBurnConfig.GENERAL.dimList.get()) // for each dim in the list.
+        {
+            if (SunBurnConfig.GENERAL.whitelistOrBlacklist.get()) // Check whitelist/blacklist state. True = Whitelist
+            {
+                // Whitelist methods. (Damage only in)
+                if (player.dimension.getId() == dim) // is the player in this dim?
+                    damageConditionCheck(player);
+            }
+            else
+            {
+                // Blacklist method. (Damage everything but)
+                if (player.dimension.getId() != dim) // is the player not in this dim?
+                    damageConditionCheck(player);
+            }
+        }
+    }
+
+    private void damageConditionCheck(PlayerEntity player)
+    {
+        // Does the player need to see they sky or do we always burn over a Y level?
+        if (SunBurnConfig.GENERAL.playerMustSeeSky.get() && player.world.canBlockSeeSky(player.getPosition()) | SunBurnConfig.GENERAL.alwaysBurnOverYLevel.get() &&
+                player.getPosition().getY() >= SunBurnConfig.GENERAL.burnOverYLevel.get())
+        {
+            damagePlayer(player); // apply damage accordingly.
+        }
+    }
+
+    private void damagePlayer(PlayerEntity player)
+    {
+        if (SunBurnConfig.GENERAL.bypassFireResist.get()) // Should we bypass the fire resist effect?
+        {
+            // if so damage them with our damage.
+            player.attackEntityFrom(damageSource, SunBurnConfig.GENERAL.bypassDamge.get());
+            player.setFire(1);
+        }
+        else player.setFire(SunBurnConfig.GENERAL.lengthOfBurn.get()); //otherwise use a regular burn.
     }
 }
