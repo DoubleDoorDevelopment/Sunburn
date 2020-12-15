220c53c2bf676a5f4ec62392f7d6afb9931acabd
Claycorp <Claycorp@doubledoordev.net>
2020-12-15 01:26:50 -0600

    Version Bump

--- src/main/java/net/doubledoordev/sunburn/Sunburn.java
+++ src/main/java/net/doubledoordev/sunburn/Sunburn.java
@@ -14,7 +14,7 @@ public class Sunburn
 {
     public static final String MOD_ID = "sunburn";
     public static final String MOD_NAME = "Sunburn";
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "2.0.1";
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
